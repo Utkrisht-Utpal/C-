6c2b56579fcb91c3d83894f3511cd4f043a33adb
Utkrisht Utpal <utkrishtutpal1@gmail.com>
2025-04-02 21:45:41 +0530

    CLasses and Objects

--- Classes and objects/1st.cpp
+++ Classes and objects/1st.cpp
@@ -2,39 +2,46 @@
 #include <string>
 using namespace std;
 
-class products
-{
+class Human{
     public:
-    string product_name;
-    int price;
-    string desc;
-    string discount; 
+    string name;
+    int age;
+    bool studying;
+    string occupation;
+    bool married;
 
-    void inprice()
+    void isEating()
     {
-        price += 100;
+        cout << "This person is now eating and then he will wash his hands." << endl;
     }
-    void diprice()
+
+    void isMarrying()
     {
-        price -= 100;
+        cout << "I think the person is going to marry soon." << endl;
+    }
+
+    void isSleeping()
+    {
+        cout << "This person is sleeping." << endl;
     }
 };
 
-int main()
+int main ()
 {
-    products p1;
-    p1.product_name = "Toy Car";
-    p1.price = 199;
-    cout << p1.price <<endl;
-    p1.desc = "This is a toy car of supermario theme.";
-    p1.discount = "30%";
-    p1.inprice();
 
-    cout << "The name of the product is: " << p1.product_name << endl;
-    cout << "The price of the product is: " << p1.price << endl;
-    cout << "The name of the product is: " << p1.product_name  << endl;
-    cout << "The decription of the product is: " << p1.desc << endl;
-    cout << "The price is discounted by: "<< p1.discount << endl;
+    Human Human1;
+    Human1.name = "Utpal";
+    Human1.age = 18;
+    Human1.married = false;
+    Human1.occupation = "Student";
+    
+    cout << Human1.name << endl;
+    cout << Human1.age << endl;
+    cout << Human1.married << endl;
+    cout << Human1.occupation << endl;
+    
+    Human1.isEating();
+    Human1.isSleeping();
 
     return 0;
 }
