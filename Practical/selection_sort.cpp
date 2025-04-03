6924308110ab636bd9374d1716573f5819978a9a
Utkrisht Utpal <utkrishtutpal1@gmail.com>
2025-04-03 19:23:33 +0530

    I practiced coding

--- Practical/selection_sort.cpp
+++ Practical/selection_sort.cpp
@@ -3,25 +3,24 @@ using namespace std;
 
 int main()
 {
-    int n = 7;
-    int arr[n] = {1, 3, 4, 7, 80, 2, 15};
+    int n = 5;
+    int arr[n] = {56, 24, 2, 4, 18};
 
     for (int i = 0; i < n - 1; i++)
     {
         int minterm = i;
-        for (int j = i + 1; j < n; j++)
+        for(int j = i + 1; j < n; j++)
         {
             if(arr[j] < arr[minterm])
             {
                 minterm = j;
             }
         }
-        swap(arr[i], arr[minterm]);
+        swap(arr[i],arr[minterm]);
     }
     
     for (int i = 0; i < n; i++)
     {
-        cout << arr[i];
-        cout << endl;
+        cout << arr[i] << endl;
     }
 }
