840d3727128587af6648cbabd02f76905870bb44
Utkrisht Utpal <utkrishtutpal1@gmail.com>
2025-04-03 19:13:08 +0530

    I practiced coding

--- Practical/bubble_sort.cpp
+++ Practical/bubble_sort.cpp
@@ -4,22 +4,22 @@ using namespace std;
 int main()
 {
     int n = 5;
-    int arr[n] = {11, 15, 2, 6, 8};
+    int arr[n] = {56, 24, 2, 4, 18};
 
-    for (int i = 0; i < n - 1; i++)
+    for(int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n - i - 1; j++)
+        for(int j = 0; j < n - i - 1; j++)
         {
-            if(arr[j] > arr[j+1])
+            if (arr[j] > arr[j+1])
             {
-                swap(arr[j],arr[j+1]);
+            swap(arr[j],arr[j+1]);
             }
         }
     }
 
+    cout << "Sorted array: " << endl;
     for (int i = 0; i < n; i++)
     {
-        cout << arr[i];
-        cout << endl;
+        cout << arr[i] << endl;
     }
 }
